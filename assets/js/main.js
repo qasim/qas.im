ea3219a3601ad379450acd2aaa8361444abb0c62
Qasim Iqbal <me@qas.im>
2015-04-03 19:12:47 -0400

    header simpler!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $(window).resize(function() {
+  /*$(window).resize(function() {
     $('.final-spacer').css({
       'height': $(window).height() - 230
     });
@@ -23,39 +23,31 @@ $(document).ready(function() {
         scrollTop: $('#' + id).offset().top - 94
       }, 300)
     }
-  })
+  })*/
 
   $(window).scroll(function() {
+
     var scroll = $(window).scrollTop()
 
-    if($(window).width() > 1000) {
-      if(scroll <= 0) {
-        setHeaderSize(96)
-      } else if(scroll > 0 && scroll < 48) {
-        setHeaderSize(96 - $(window).scrollTop())
-      } else if (scroll >= 48) {
-        setHeaderSize(48, true)
-      }
-    } else {
-      if(scroll <= 0 || $(window).width() <= 560) {
-        $('.header .title').css('visibility', 'visible')
-        $('.header .title').css({
-          'height': 96,
-          'opacity': 1
-        })
-      } else if(scroll > 0 && scroll <= 96) {
-        $('.header .title').css({
-          'height': 96 - scroll,
-          'opacity': 1 - (scroll / 96)
-        })
-      } else if(scroll > 96) {
-        $('.header .title').css({
-          'height': 0,
-          'opacity': 0
-        })
-      }
+    if(scroll <= 0) {
+      $('.header .title').css('visibility', 'visible')
+      $('.header .title').css({
+        'height': 96,
+        'opacity': 1
+      })
+    } else if(scroll > 0 && scroll <= 96) {
+      $('.header .title').css({
+        'height': 96 - scroll,
+        'opacity': 1 - (scroll / 96)
+      })
+    } else if(scroll > 96) {
+      $('.header .title').css({
+        'height': 0,
+        'opacity': 0
+      })
     }
 
+    /*
     var about_bottom = $('#about').offset().top + $('#about').height() - 48
     //var resume_bottom = $('#resume').offset().top + $('#resume').height() - 48
     var projects_bottom = $('#projects').offset().top + $('#projects').height() - 48
@@ -66,13 +58,15 @@ $(document).ready(function() {
     } /*else if(scroll >= about_bottom && scroll < resume_bottom) {
       $('.item').removeClass('active')
       $('#_resume').addClass('active')
-    }*/ else if(scroll >= about_bottom) { //change about to resume
+    }*\/ else if(scroll >= about_bottom) { //change about to resume
       $('.item').removeClass('active')
       $('#_projects').addClass('active')
     }
+    */
   })
 })
 
+/*
 function setHeaderSize(height, bool) {
   $('.header').css({
     'height': height + 'px',
@@ -92,4 +86,4 @@ function setHeaderSize(height, bool) {
       'font-size': Math.ceil(height * 0.75) + 'px'
     })
   }
-}
+}*/
